115d8708873ebf1a959eb47fd45438253b24b783
Ariful Islam <engr.arifulislam777@gmail.com>
2024-01-01 13:16:26 +0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function( $ ){
     // Your code in here
+
+//Panel Opening Event
 	
 	$("#menuicon").click(function(){
 		$("#popwrap").addClass("new");
 	});
 	
-	 
+// Pnale Close Event	 
 	$("#closeicon").click(function() {
 			$("#menucol").animate({
 			'margin-left' : "50%",
@@ -18,15 +20,18 @@ jQuery(document).ready(function( $ ){
 		//$("#popwrap").removeClass("new");
 	
 		 });
-	 
+
+
+//Destroy CSS and class//
+	
 	$("#closeicon").click (function() {
         $("#popwrap").delay(1000).queue(function() {  // Wait for 1 second.
             $("#popwrap").removeClass("new").dequeue();
 			$("#menucol").removeAttr("style");
 			$("#imgcol").removeAttr("style");
-        });
-    }
-);
+	        });
+	    }
+	);
 
 	
 	
